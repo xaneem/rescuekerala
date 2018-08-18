37146d715dc36610d83fb98f4e1b27b67712f4de
Arunkrishnan <hereisarunkrishnan@gmail.com>
2018-08-18 20:20:24 +0530

    bug fix

--- mainapp/redis_queue.py
+++ mainapp/redis_queue.py
@@ -4,5 +4,5 @@ from redis import Redis
 from rq import Queue
 
 redis_host = os.environ.get("REDIS_URL")
-redis_port = os.environ.get("mainapp/views.py")
+redis_port = os.environ.get("REDIS_PORT")
 q = Queue(connection=Redis(host=redis_host, port=redis_port))
