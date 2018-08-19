a820e35cc78b7556310906fb0c6260bd846ffbf7
Saneem Perinkadakkat <xaneem@gmail.com>
2018-08-19 14:21:14 +0530

    Facilities available moved to camp details

--- mainapp/views.py
+++ mainapp/views.py
@@ -357,8 +357,7 @@ class CampRequirementsForm(forms.ModelForm):
         'clothing_req',
         'sanitary_req',
         'medical_req',
-        'other_req',
-        'facilities_available'
+        'other_req'
         ]
        read_only = ('name',)
        widgets = {
@@ -396,6 +395,7 @@ class CampDetailsForm(forms.ModelForm):
         'taluk',
         'village',
         'contacts',
+        'facilities_available',
         'map_link',
         'latlng',
         ]
